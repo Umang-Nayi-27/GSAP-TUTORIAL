cb32a6f1cfded62005095a2118e0827ee90adb3e
Umang-Nayi-27 <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-07-11 00:07:57 +0530

    Update scrolltriger.js

--- scrolltriger.js
+++ scrolltriger.js
@@ -1,11 +1,23 @@
+gsap.to(".circle1",{ 
+                    x:200 ,
+        
+                    scrollTrigger:{
+                                    trigger : ".circle1",
+                                    start: "top 30%",   // circle2 nu top(uper no bhag) .....  page na uper na bhage thi 90% niche poche tyare chalu thay...
+                                    end:"bottom 10%",  // circle2 nu bottom(niche no bhag)...... page na uper na bhage thi 50% niche poche tyare pati jay...
+                                    scrub:0.5           // after animation ma smothness ane sensitivity aalva mate
+                                  }
+    })
+
+
 gsap.to(".circle2",{ 
                     x:750 ,
                     
                     scrollTrigger:{
                                     trigger : ".circle2",
-                                    start: "top 90%",   // circle2  90% visible thay tayre animation start
-                                    end:" top 50%",     // circle2 20% vible rai jay tyare animation bandh
-                                    scrub:0.5
+                                    start: "top 90%",   // circle2 nu top(uper no bhag)... page na uper na bhage thi 90% niche poche tyare chalu thay...
+                                    end:" top 50%",     // circle2 nu top(uper no bhag).... page na uper na bhage thi 50% niche poche tyare pati jay...
+                                    scrub:0.5           // after animation ma smothness ane sensitivity aalva mate
                                   }
                     })
 
