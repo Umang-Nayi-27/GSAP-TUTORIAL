cbaf9e6435b5440fcc7701cdae073e053a6554b5
Umang-Nayi-27 <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-07-10 23:59:54 +0530

    Add files via upload

---
+++ scrolltriger.js
@@ -0,0 +1,12 @@
+gsap.to(".circle2",{ 
+                    x:750 ,
+                    
+                    scrollTrigger:{
+                                    trigger : ".circle2",
+                                    start: "top 90%",   // circle2  90% visible thay tayre animation start
+                                    end:" top 50%",     // circle2 20% vible rai jay tyare animation bandh
+                                    scrub:0.5
+                                  }
+                    })
+
+                    
